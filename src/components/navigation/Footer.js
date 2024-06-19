c11abc2dbeea9a4097dde1321b1db6aeec886889
Jr Mohan <jmohan6@myseneca.ca>
2024-06-19 11:20:19 -0400

    Footer barebones done

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom"
-import { IconContext } from "react-icons";
-import { HiMenu } from "react-icons/hi";
 import { useState, useEffect, useRef } from "react";
+
+import { FaFacebook, FaInstagram, FaLinkedin} from "react-icons/fa";
 import logo from "../../RoughAssets/WhiteLogoNoTree.svg"
 
 export default function Footer(){
@@ -23,52 +23,76 @@ export default function Footer(){
     };
 
     return(
-        <header className="bg-neutral-800 text-white h-[4rem] px-3 sm:px-8 flex justify-between items-center z-40 w-full">
-            {check ? 
-                <>
-                    <div className="bg-neutral-950 w-screen h-screen absolute left-0 top-0 opacity-40 sm:hidden"></div>
-                    <div className="text-base text-white text-left absolute left-0 top-0 z-40 w-3/4 p-6 h-screen bg-neutral-900 opacity-100 :hidden" ref={newRef}>
-                        <div className="">
-                            <img src={logo} className="h-16"/>
-                        </div>
-                        <button 
-                            className="text-left  w-full py-3 pr-3  text-2xl my-1">
-                            Home
-                        </button>
-                        {navArray.map((item, index) => (
-                            <button 
-                                className="text-left  w-full py-3 pr-3  text-2xl my-1" key={index}>
-                                    {item.name}
-                            </button>
-                        ))}
-                    </div> 
-                </>
-                :
-                null
-            }
-            <div className="">
-                <img src={logo} className="h-16"/>
-            </div>
-            <div className="sm:flex hidden justify-between">
-                {/* <p><NavLink to="/">Products</NavLink></p>
-                <p><NavLink to="/">Buisness</NavLink></p>
-                <p><NavLink to="/">Guides</NavLink></p>
-                <p><NavLink to="/">Contact</NavLink></p> */}
-                {navArray.map((item, index) => (
-                    <button 
-                        className="text-left  w-full py-3 pr-3 my-1" key={index}>
-                            {item.name}
-                    </button>
-                ))}
-            </div>
-            <IconContext.Provider value={{ className: 'text-zinc-500 text-xl size-10 opacity-100  hover:text-slate-200 hover:cursor-pointer transition duration-100' }}>
-                <div className="sm:hidden flex" onClick={() => {setCheck(true)}}>
-                    <HiMenu/>
+        <footer className="bg-neutral-800 text-white z-40 w-full pt-12 px-3 sm:px-8">
+
+            {/* Top */}
+            <div className="flex justify-between h-48">
+
+                {/* About */}
+                <div className="w-full gap-y-4">
+                    <h1>About</h1>
+                    
+                    <p>About Us</p>
+                    <p>Our Solution</p>
+                    <p>Our History</p>
+
                 </div>
-            </IconContext.Provider>
-            <p className="text-right sm:flex hidden ">
-                EN
-            </p>
-        </header>
+
+                {/* Products */}
+                <div className="w-full">
+                    <h1>Products</h1>
+
+                    <p>BTR Series</p>
+                    <p>DTR Series</p>
+                    <p>CFC Series</p>
+                    <p>MHR Series</p>
+                    <p>DDR Series</p>
+                    
+                </div>
+
+                {/* Resources */}
+                <div className="w-full">
+                    <h1>Resources</h1>
+
+                    <p>Owner's Guide</p>
+                    <p>Maintnence Guide</p>
+                    <p>Simplified Thermostat Guides</p>
+                    <p>Standard Limited Warranty</p>
+ 
+                </div>
+
+                {/* Contact */}
+                <div className="w-full">
+                    <h1>Contact</h1>
+
+                    <p>Subscribe to our newsletter to keep up to date with all things Aurum!</p>
+
+                    <div class="mt-4 mb-3 xl:pl-3 inline-block md:max-w-xl w-full overflow-hidden border bg-aurumgreen-200 border-gray-200 rounded-sm focus-within:ring focus-within:ring-aurumgreen-300"><div class="flex flex-wrap items-center"><div class="w-full xl:flex-1"><input class="p-3 xl:p-0 xl:pr-6 w-full text-sm font-roboto-regular bg-aurumgreen-200 text-gray-500 placeholder-gray-500 outline-none" id="footerInput1-1" type="text" placeholder="Enter your e-mail address"/></div><div class="w-full xl:w-auto"><div class="block"><button class="py-2 px-8 w-full text-white font-productsans-medium border border-aurumgreen-700 focus:ring focus:ring-aurumgreen-300 bg-aurumgreen-600 hover:bg-aurumgreen-700 transition ease-in-out duration-200" type="button">Subscribe</button></div></div></div></div>
+
+                </div>
+
+            </div>
+
+            {/* Divide */}
+            <hr/>
+
+            {/* Bottom */}
+            <div className="flex justify-between items-center py-8">
+                {/* Logo */}
+                <div className="">
+                    <img src={logo} className="h-16"/>
+                </div>
+
+                {/* Copyright */}
+                <p>Copyright © 2024 Aurum Manufacturing Inc. All Rights Reserved</p>
+
+                {/* Socials */}
+                <div className="flex gap-4">
+                    <FaFacebook/>
+                    <FaInstagram/>
+                    <FaLinkedin/>
+                </div>
+            </div>
+        </footer>
     )
 }
