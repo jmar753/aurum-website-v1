dec7cfe5e5f104fe6ddf5d3fb943c5f1fd49b98e
Jr Mohan <jmohan6@myseneca.ca>
2024-07-11 17:58:49 -0400

    push up

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -191,7 +191,7 @@ export default function Header(){
 
     return(
         <motion.nav 
-            className="bg-naturegreen-800/90 backdrop-blur-md shadow-lg shadow-slate-900/20 shadow-b-2 text-naturegreen-800 h-[4rem] px-4 fixed top-0 z-40 w-screen flex justify-center items-center"
+            className="bg-naturegreen-800/90 backdrop-blur-md shadow-lg shadow-slate-900/20 shadow-b-2 text-naturegreen-800 h-[4rem] px-4 fixed -top-1 z-40 w-screen flex justify-center items-center"
             variants={{
                 visible: {y: 0},
                 hidden: {y: "-100%"},
