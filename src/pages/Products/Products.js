e2fa10e991390fd64a22f42483fc39dfeb436a3f
Jr Mohan <jmohan6@myseneca.ca>
2024-07-18 11:07:58 -0400

    git push image

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,9 +1,11 @@
+import { RetrofitData } from "../../data/RetrofitData";
+
 export default function Products() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-neutral-700 via-neutral-400 to-neutral-900">
-      <div className='w-full h-full max-w-7xl grid grid-cols-5'>
+    <div className="max-h-screen h-screen flex flex-col items-center justify-center bg-gradient-to-b from-neutral-700 via-neutral-400 to-neutral-900">
+      <div className='w-full h-full max-w-7xl grid grid-cols-4'>
         {/* Cards */}
-        <div className='col-span-2 h-full grid grid-cols-1 gap-4 overflow-y-auto hide-scrollbar'>
+        <div className='col-span-1 h-full grid grid-cols-1 gap-4 overflow-y-auto hide-scrollbar'>
           <div className='w-full aspect-w-1 aspect-h-1 rounded-lg bg-gray-600 border-white flex justify-center items-center'>Box</div>
           <div className='w-full aspect-w-1 aspect-h-1 rounded-lg bg-gray-600 border-white flex justify-center items-center'>Box</div>
           <div className='w-full aspect-w-1 aspect-h-1 rounded-lg bg-gray-600 border-white flex justify-center items-center'>Box</div>
@@ -12,8 +14,8 @@ export default function Products() {
         </div>
 
         {/* Image */}
-        <div className='col-span-3'>
-          {/* Add your image or content here */}
+        <div className='col-span-3 flex justify-center items-center'>
+          <img src={RetrofitData[0].images[0]} className="h-[300px]"></img>
         </div>
 
       </div>
