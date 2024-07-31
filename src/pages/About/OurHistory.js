b83ab53e165dda45c421009ba2a078f6cd9439af
Jr Mohan <jmar7533@gmail.com>
2024-07-31 12:39:49 -0400

    OurHistory ready for review

--- src/pages/About/OurHistory.js
+++ src/pages/About/OurHistory.js
@@ -1,4 +1,5 @@
 import Reveal from "../../animations/Reveal";
+import tree from "../../RoughAssets/TreeGold.svg"
 
 export default function OurHistory(){
     const pattents = [
@@ -82,79 +83,112 @@ export default function OurHistory(){
 					</div>
 				</div>
 
-				<div className="space-y-8 py-6 bg-leafgreen rounded-3xl" >
+				<div className="space-y-8 p-4 mx-8 mt-8 mb-24 border-leafgreen border-2 rounded-3xl" >
 					<p className="text-base lg:text-xl">He designed and oversaw production of 43 container boiler rooms for the United States Army. He designed and manufactured 6 boilers that were installed in Russia with 50,000,000 BTU input. He educated technicians on how to install a 7-pipe vertical fan coil unit in California. He assisted in the designing of Henex stainless steel heat exchangers in Austria. </p>
 				</div>
 
 
 				{/* Patents */}
-				<div>
-					<h1 className="pt-24 pb-32 text-3xl text-center">Vittorio's officially patented designs </h1>
-				</div>
-
-				{pattents.map((item, index) => (
+				<div className="bg-aurumgrey">
 					<div>
-						<p>{item.title}</p>
-						<p><span className="font-bold">Pattent Number</span>{item.patNumber}</p>
-						<p><span className="font-bold">Abstract:</span>{item.abstract}</p>
-						<p><span className="font-bold">Filed:</span>{item.filed}</p>
-						<p><span className="font-bold">Date of Patent:</span>{item.DoP}</p>
-						<p><span className="font-bold">Inventors:</span>{item.inventors}</p>
+						<h1 className="pt-24 pb-12 px-4 text-3xl text-center">Vittorio's officially patented designs: </h1>
 					</div>
-				))}
 
-				{/* QnA */}
-				<div className="space-y-8 py-6" >
-					<p className="text-base lg:text-xl">Vittorio was and still is very respected through the plumbing and HVAC industry as many engineers, developers and designers still look to him for guidance and advice. He continues his legacy in the mechanical industry by consulting our design and research teams at Aurum Manufacturing.</p>
+					{pattents.map((item, index) => (
+						<div className="space-y-8 p-4 m-8 bg-aurumblack rounded-3xl">
+							<p className="font-bold text-lg text-aurumgold-500">{item.title}</p>
+							<p className="text-neutral-300"><span className="font-bold text-white">Pattent Number: </span>{item.patNumber}</p>
+							<p className="text-neutral-300"><span className="font-bold text-white">Abstract: </span>{item.abstract}</p>
+							<p className="text-neutral-300"><span className="font-bold text-white">Filed: </span>{item.filed}</p>
+							<p className="text-neutral-300"><span className="font-bold text-white">Date of Patent: </span>{item.DoP}</p>
+							<p className="text-neutral-300"><span className="font-bold text-white">Inventors: </span>{item.inventors}</p>
+						</div>
+					))}
 				</div>
 
+				<div className="px-8 pb-12 border-b relative">
+					<div className="space-y-8 py-6" >
+						<p className="text-base lg:text-xl">Vittorio was and still is very respected through the plumbing and HVAC industry as many engineers, developers and designers still look to him for guidance and advice. He continues his legacy in the mechanical industry by consulting our design and research teams at Aurum Manufacturing.</p>
+					</div>
+
+					<div className="space-y-8 py-6" >
+						<p className="text-base lg:text-xl">Not only were Vittorio’s boilers successful in Canada and The United States, the sales also expanded internationally. Vittorio was flying to countries such as Russia, Italy, Belgium and even Austria to educate and help engineers with his boilers. </p>
+					</div>
+
+					<div className="absolute -bottom-3 right-6 h-6 w-6 border-2 border-white bg-aurumblack rounded-full flex justify-center items-center"/>
+				</div>
 				
 				{/* QnA */}
-				<div className="space-y-8 py-6" >
-					<p className="text-base lg:text-xl">Not only were Vittorio’s boilers successful in Canada and The United States, the sales also expanded internationally. Vittorio was flying to countries such as Russia, Italy, Belgium and even Austria to educate and help engineers with his boilers. </p>
-				</div>
+				<div className="px-8 pb-12">
+					<div className="pt-24 pb-10">
+						<h1 className="text-3xl">Questions and Answers</h1>
+						<h1 className="text-neutral-300">With Vittorio Zorzit</h1>
+					</div>
 
-				<div className="py-6 grid grid-cols-2" >
-					<p className="text-2xl">“What is your greatest success?”</p>
-					<p className="text-base lg:text-xl">It’s hard to narrow down my greatest success. The thing is when you do things, to me it didn’t sound like a big deal. It was a big deal for someone who didn’t know anything about it. I was one of the few leaders in the industry while being honest.</p>
-				</div>
+					<div className="py-6">
+						<div className="flex w-full text-xl text-white gap-x-3">
+							<p className="w-4">1.</p>
+							<div className="space-y-4">
+								<p>“What is your greatest success?”</p>
+								<p className="text-base lg:text-xl text-neutral-50">It’s hard to narrow down my greatest success. The thing is when you do things, to me it didn’t sound like a big deal. It was a big deal for someone who didn’t know anything about it. I was one of the few leaders in the industry while being honest.</p>
+							</div>
+						</div>
+					</div>
 
-				<div className="py-6 grid grid-cols-2" >
-					<p className="text-2xl">“What’s one of the most challenging obstacles you went through?”</p>
-					<p className="text-base lg:text-xl">Probably the most annoying thing was sitting down with a patent lawyer. Every little thing that would take up a quarter of a page they wrote 20 pages for. I would be still reading today if I read it all.</p>
+					<div className="py-6 space-y-4" >
+						<div className="flex w-full text-xl text-white gap-x-3">
+							<p className="w-4">2.</p>
+							<div className="space-y-4">
+								<p>“What’s one of the most challenging obstacles you went through?”</p>
+								<p className="text-base lg:text-xl text-neutral-300">Probably the most annoying thing was sitting down with a patent lawyer. Every little thing that would take up a quarter of a page they wrote 20 pages for. I would be still reading today if I read it all.</p>
+							</div>
+						</div>
+					</div>
 				</div>
 
 				{/* Second Part */}
-				<div className="" >
-					<h1 className="text-4xl lg:text-7xl text-gray-100 ">The Introduction of Aurum Manufacturing</h1>
+				<div className="px-8">
+					<div className="pt-12" >
+						<h1 className="text-4xl lg:text-7xl text-gray-100 ">The Introduction of Aurum Manufacturing</h1>
+					</div>
+
+					<div className="space-y-8 py-6" >
+						<p className="text-base lg:text-xl">Adam Zorzit began his mechanical experience at the age of 16. After 10 years of servicing for a manufacturing company under the guidance of his father, Adam began to research a solution for outdated fan coil systems. With sparked interest and determination, Adam set off to manufacture and install newly designed retrofit models. Adam’s visionary retrofit design production began in his home garage. </p>
+					</div>
+
+					<div className="space-y-8 py-6" >
+						<p className="text-base lg:text-xl">After many small success stories and when production began to grow, Adam was inspired and motivated to build Aurum with his sister, Stacy. Together, Stacy and Adam continued to grow Aurum by purchasing a manufacturing building, hiring staff and building close relationships with new trusted suppliers, one of which is their brother Richard. </p>
+					</div>
+
+					<div className="space-y-8 py-6" >
+						<p className="text-base lg:text-xl">After many small success stories and when production began to grow, Adam was inspired and motivated to build Aurum with his sister, Stacy. Together, Stacy and Adam continued to grow Aurum by purchasing a manufacturing building, hiring staff and building close relationships with new trusted suppliers, one of which is their brother Richard. </p>
+					</div>
+
+					<div className="space-y-8 py-6 flex flex-col items-center justify-center" >
+						<img src="https://www.aurum-m.ca/wp-content/themes/twentyseventeen-child/assets/images/Adam_and_Stacy.png"></img>
+						<p className="text-neutral-300 text-sm">Founders Adam (Right) and Stacy Zorzit (Middle) with their brother Richard Zorzit (Left), 2017</p>
+					</div>
+
+					<div className="space-y-8 py-6" >
+						<p className="text-base lg:text-xl">Adam and Stacy are determined to lead Aurum and the entire HVAC industry with the same drive, purpose and honesty as their father led before them. The name Aurum was not randomly selected. With the knowledge from their father, there was one family member whose encouragement and support could not go without recognition. Therefore, they believed to properly show their appreciation and love for their mother Ornella, they decided to name the company after her. </p>
+					</div>
 				</div>
 
-				<div className="space-y-8 py-6" >
-					<p className="text-base lg:text-xl">Adam Zorzit began his mechanical experience at the age of 16. After 10 years of servicing for a manufacturing company under the guidance of his father, Adam began to research a solution for outdated fan coil systems. With sparked interest and determination, Adam set off to manufacture and install newly designed retrofit models. Adam’s visionary retrofit design production began in his home garage. </p>
-				</div>
-
-				<div className="space-y-8 py-6" >
-					<p className="text-base lg:text-xl">After many small success stories and when production began to grow, Adam was inspired and motivated to build Aurum with his sister, Stacy. Together, Stacy and Adam continued to grow Aurum by purchasing a manufacturing building, hiring staff and building close relationships with new trusted suppliers, one of which is their brother Richard. </p>
-				</div>
-
-				<div className="space-y-8 py-6" >
-					<p className="text-base lg:text-xl">After many small success stories and when production began to grow, Adam was inspired and motivated to build Aurum with his sister, Stacy. Together, Stacy and Adam continued to grow Aurum by purchasing a manufacturing building, hiring staff and building close relationships with new trusted suppliers, one of which is their brother Richard. </p>
-				</div>
-
-				<div className="space-y-8 py-6 flex flex-col items-center justify-center" >
-					<img src="https://www.aurum-m.ca/wp-content/themes/twentyseventeen-child/assets/images/Adam_and_Stacy.png"></img>
-					<p>Founders Adam (Right) and Stacy Zorzit (Middle) with their brother Richard Zorzit (Left), 2017</p>
-				</div>
-
-				<div className="space-y-8 py-6" >
-					<p className="text-base lg:text-xl">Adam and Stacy are determined to lead Aurum and the entire HVAC industry with the same drive, purpose and honesty as their father led before them. The name Aurum was not randomly selected. With the knowledge from their father, there was one family member whose encouragement and support could not go without recognition. Therefore, they believed to properly show their appreciation and love for their mother Ornella, they decided to name the company after her. </p>
-				</div>
-
-				<div className="space-y-8 py-6 bg-aurumaqua-secondary rounded-3xl" >
-					<h1>Ornella</h1>
-					<p>From Wikipedia, the free encyclopedia</p>
+				<div className="space-y-8 p-4 mx-8 mt-8 mb-24 border-leafgreen border-2 rounded-3xl relative">
+					<div>
+						<h1 className="border-b">Ornella</h1>
+						<p className="text-xs text-neutral-300">From Wikipedia, the free encyclopedia</p>
+					</div>
 					<p><span className="font-bold">Ornella</span> is an Italian name. It means flowering ash tree and is a female's name. Ornella is derived from the term " aurum ," the Roman equivalent of "gold" . It is a fairly recent name and it was used the first time by Gabriele D'Annunzio in his book "The daughter of Iorio" in 1904.</p>
+
+					<img src={tree} className="h-full w-full opacity-15 absolute inset-0 "/>
 				</div>
+
+				{/* <div className="space-y-8 py-6 flex flex-col items-center justify-center" >
+					<img src="https://www.aurum-m.ca/wp-content/themes/twentyseventeen-child/assets/images/Adam_and_Stacy.png"></img>
+					<p className="text-neutral-300 text-sm">Founders Adam (Right) and Stacy Zorzit (Middle) with their brother Richard Zorzit (Left), 2017</p>
+				</div> */}
+
 			</div>
 		</>
     )
